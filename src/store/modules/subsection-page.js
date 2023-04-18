178f90f23f7df1f30d315698b340a70bbf719105
Константин Дорохов <kostya.dorokhov.2016@mail.ru>
2023-04-18 20:25:54 +0300

    Store configured

---
+++ src/store/modules/subsection-page.js
@@ -0,0 +1,45 @@
+import api from '@/api'
+
+export default {
+  namespaced: true,
+
+  state: {
+    loaded: false,
+    data: null,
+  },
+
+  getters: {
+    loaded (state) {
+      return state.loaded
+    },
+    sections (state) {
+      return state.data?.sections
+    },
+  },
+
+  actions: {
+    async fetchData ({ commit }, subsection) {
+      try {
+        const data = await api.getSubsectionData(subsection)
+        commit('setData', data)
+      } finally {
+        commit('setLoaded')
+      }
+    },
+  },
+
+  mutations: {
+    setData (state, data) {
+      state.data = data
+    },
+
+    setLoaded (state) {
+      state.loaded = true
+    },
+
+    resetState (state) {
+      state.loaded = false
+      state.data = null
+    },
+  },
+}
